db87794d3654bd3e3730004ccdcbec39466141dc
Basil Pulikuth <basilxg321@gmail.com>
2022-01-25 21:44:13 +0530

    Questions CSS added

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,32 @@ function scrollHeader() {
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+/*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll(".questions__item");
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".questions__content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};

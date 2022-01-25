557e0ba4f637fe42651cb03ff4d2cfb2df0d6ee8
Basil Pulikuth <basilxg321@gmail.com>
2022-01-25 22:26:01 +0530

    Scroll/Reveal animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,3 +135,21 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__img`, { delay: 500 });
+sr.reveal(`.home__social`, { delay: 600 });
+sr.reveal(`.about__img, .contact__box`, { origin: "left" });
+sr.reveal(`.about__data, .contact__form`, { origin: "right" });
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
+  interval: 80,
+});

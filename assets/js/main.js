25fb4d8f50a5de0c52ae1baecb8fcdd16de45e2c
Basil Pulikuth <basilxg321@gmail.com>
2022-01-25 22:07:48 +0530

    Scroll Up added

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,11 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)

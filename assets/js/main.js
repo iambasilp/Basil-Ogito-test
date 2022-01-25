3b2a808182083d1c34e8bb2b84a23e2cd75991e3
Basil Pulikuth <basilxg321@gmail.com>
2022-01-25 22:00:46 +0530

    Scroll section Active Link CSS added

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,27 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
